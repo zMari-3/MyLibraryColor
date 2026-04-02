d99e40831ec4fc32015f417f114e4f46fe1e601c
Maria Zybina <zybina.2003@mail.ru>
2026-04-02 12:49:54 +0300

    Create

--- MyLibraryColors/obj/Debug/net8.0/MyLibraryColors.AssemblyInfo.cs
+++ MyLibraryColors/obj/Debug/net8.0/MyLibraryColors.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Color library ...")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41010185ec998a394304c57452f7601fb7b263a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLibraryColors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLibraryColors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
